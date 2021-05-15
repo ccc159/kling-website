c1cf72f3868fb22a3c36f9c6b2721de3de546ff4
Jingcheng Chen <mail@chenjingcheng.com>
2021-05-15 21:57:09 +0200

    updated image quality

--- src/common/SvgIcon/index.js
+++ src/common/SvgIcon/index.js
@@ -1,7 +1,18 @@
 const SvgIcon = ({ src, width, height, style }) => (
   <div style={{ maxHeight: '400px', display: 'flex', justifyContent: 'center' }}>
-    {/* eslint-disable-next-line react/jsx-no-duplicate-props */}
-    <img src={`/img/svg/${src}`} alt={src} with={width} height={height} style={style} style={{ maxHeight: '400px' }} />
+    <img
+      autoCorrect
+      src={`/img/svg/${src}`}
+      alt={src}
+      with={width}
+      height={height}
+      style={style}
+      // eslint-disable-next-line react/jsx-no-duplicate-props
+      style={{
+        maxHeight: '400px',
+        imageRendering: '-webkit-optimize-contrast',
+      }}
+    />
   </div>
 );
 
