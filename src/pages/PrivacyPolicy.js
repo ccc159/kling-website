d9021720a5671e8cdf20c88a7553c5b5f986fa89
Jingcheng Chen <mail@chenjingcheng.com>
2021-05-17 10:53:31 +0200

    updated privacy policy

--- src/pages/PrivacyPolicy.js
+++ src/pages/PrivacyPolicy.js
@@ -6,11 +6,11 @@ const PrivacyPolicy = () => {
       <figure style={{ margin: '100px 10px' }}>
         <h1>Privacy Policy</h1>
         <p>
-          Kling takes your privacy very seriously. It uses no third-party analytics or advertising frameworks. Kling logs no information on you and
-          has no interest in doing such.
+          Kling takes your privacy very seriously. It uses expo framework in react native development, which may provide thrid-party Ads SDKs. But in
+          our app we never called any of those APIs. Kling logs no information on you and has no interest in doing such.
           <br />
           <br />
-          <b>Kling does not collect, transmit, distribute or sell your data.</b>
+          <b>Kling does not collect, store, transmit, distribute or sell your data.</b>
         </p>
       </figure>
     </Container>
